80a3d3f7b56b9238efbfb474878b3682e7d309b8
arknfel <most.kaseb@gmail.com>
2022-04-24 08:26:09 +0200

    added frontend build and deployment

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -7,9 +7,9 @@ POSTGRES_PASSWORD=postgres \
 POSTGRES_DB=udagram \
 DB_PORT=5432 \
 POSTGRES_HOST=udagram-db.cu8ywbspb3t6.us-east-1.rds.amazonaws.com \
-AWS_REGION=us-east-1 \
-AWS_BUCKET=randomsat-b01 \
-AWS_PROFILE=default \
 JWT_SECRET=U\$hal!notPA\$s! \
 URL=http://udagram-api-app-dev.us-east-1.elasticbeanstalk.com/api/v0
+AWS_REGION=us-east-1 \
+AWS_BUCKET=randomsat-b01 \
+AWS_PROFILE=default
 eb deploy
