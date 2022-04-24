6d4f56aead33b5eb9899437f130d2df90ce7a989
arknfel <most.kaseb@gmail.com>
2022-04-24 02:13:32 +0200

    added frontend build and deployment

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,4 +2,9 @@ echo hi we are at: $(pwd)
 ls -la
 eb init udagram-api-app
 eb use udagram-api-app-dev
+eb setenv POSTGRES_USER=postgres \
+POSTGRES_PASSWORD=postgres \
+POSTGRES_DB=udagram \
+DB_PORT=5432 \
+POSTGRES_HOST=udagram-db.cu8ywbspb3t6.us-east-1.rds.amazonaws.com
 eb deploy
